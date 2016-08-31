30f2d3c192e0b1e219d107aed4a58c2f74011e9a
jingpengwu <jingpeng.wu@gmail.com>
2016-08-31 13:58:53 -0400

    change variable name for clearance

--- src/mst.jl
+++ src/mst.jl
@@ -12,11 +12,11 @@ export mst
 * `max_segid`: largest ID in region graph
 * `regiontree`: *maximal* spanning tree (MST) of region graph as list of edges, array of `(weight,id1,id2)` tuples. The vertices in each edge are ordered so that `id2` is unique across edges. In other words, id1 and id2 correspond to parent and child in the tree. The code places the root of the tree at segid=1
 
-The MST effectively represents the dendrogram for single-linkage
+The MST effectively represents the segmentPairsrogram for single-linkage
 clustering.  Each edge `(weight,id1,id2)` in the MST represents an
-internal vertex of the dendrogram located at height = `weight`, i.e.,
+internal vertex of the segmentPairsrogram located at height = `weight`, i.e.,
 a merging of two clusters with score `weight`.  The MST contains a bit
-more information than the dendrogram, because `id1` and `id2` are the
+more information than the segmentPairsrogram, because `id1` and `id2` are the
 root IDs of the two clusters, i.e., the maximum weight edge between
 the two clusters is between their elements `id1` and `id2`.
 
